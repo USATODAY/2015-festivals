4fa231e25fa78ecceeae97d5e5b6486a69160e61
mitchthorson <mthorson@usatoday.com>
2015-04-13 13:13:05 -0400

    add query string detection to remove header

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ define([
   'views/appView',
   'dataManager',
   'jquery_ui_touch_punch'
-  ], 
+  ],
   function(require, jQuery, Isotope, _, Backbone, config, appView, dataManager) {
 
 
@@ -27,7 +27,7 @@ define([
             if (config.isTablet || config.isMobile) {
                 $('.iapp-page-wrap').addClass('iapp-touch-device');
             }
-            
+
             //turn resize and scroll into Backbone events
             $(window).on('resize', function(e) {
                 Backbone.trigger('window:resize');
@@ -37,16 +37,37 @@ define([
                 Backbone.trigger('window:scroll');
             });
 
+            //check for no-header query string
+
+            function getQueryVariable(variable) {
+                var query = window.location.search.substring(1);
+                var vars = query.split('&');
+                for (var i = 0; i < vars.length; i++) {
+                    var pair = vars[i].split('=');
+                    if (decodeURIComponent(pair[0]) == variable) {
+                        return decodeURIComponent(pair[1]);
+                    } 
+                }
+                return false
+                console.log('Query variable %s not found', variable);
+            }
+
+            console.log(getQueryVariable("windet"));
+            if (getQueryVariable("native") == "true") {
+                console.log('hide header');
+                $('header').hide();
+                $('body').addClass('in-app')
+            }
 
             //Make data request
-          
+
             dataManager.getData();
 
             //Create app view
             appview = new appView();
-          
+
         }
-      );   
+      );
     }
   };
 });
